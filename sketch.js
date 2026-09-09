ccffbb377a31de0ca16fac95afb5446c7adc241a
Jiwon <121710047+parkdui@users.noreply.github.com>
2026-09-09 10:59:06 +0200

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // background(0);
+}
+
+function draw() {
+  background(0);
+
+  textAlign(CENTER);
+  textSize(50);
+  fill(0,255,0);
+  text("Jiwon", mouseX, mouseY);
+}
